eaf2aefabffcbfee7400e91c320c8e6856f0e1b6
LitvE <yevgen.litvinov1980@gmail.com>
2022-03-26 12:26:21 +0200

    Added app.js and server.js

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = require('./router');
+const cors = require('cors');
+const handlerError = require('./handlerError/handler');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api', router);
+app.use(handlerError);
+
+module.exports = app;
