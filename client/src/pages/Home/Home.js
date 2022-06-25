ae10f51b92cdd084feed2492a96ef49efdc6d36f
LitvE <yevgen.litvinov1980@gmail.com>
2022-06-25 21:04:48 +0300

    Dashboard button refactoring on Home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -25,7 +25,7 @@ const Home = (props) => {
     };
   });
 
-  const { isFetching } = props;
+  const { isFetching, data } = props;
   const text = CONSTANTS.HEADER_ANIMATION_TEXT[index % CONSTANTS.HEADER_ANIMATION_TEXT.length];
   return (
     <>
@@ -44,9 +44,11 @@ const Home = (props) => {
                 Or, explore our hand-picked collection of premium names
                 available for immediate purchase
               </p>
-              <div className={styles.button}>
-                <Link className={styles.button__link} to="/dashboard">DASHBOARD</Link>
-              </div>
+              {data && 
+                <div className={styles.button}>
+                  <Link className={styles.button__link} to="/dashboard">DASHBOARD</Link>
+                </div>
+              }
             </div>
             <div className={styles.greyContainer}>
               <SlideBar
@@ -68,7 +70,7 @@ const Home = (props) => {
                     name ideas from world's largest community of naming experts.
                     With 75,000+ creatives and 15,000+ successful naming projects,
                     Squadhelp is by far the largest naming platform across the globe .
-</p>
+                  </p>
                 </div>
                 <div className={styles.card}>
                   <img
@@ -81,7 +83,7 @@ const Home = (props) => {
                     we ensure that you receive more ideas from our top-quality creatives,
                     and Gamification best practices ensure two-way communication throughout your
                     contest.
-</p>
+                  </p>
                 </div>
                 <div className={styles.card}>
                   <img
@@ -94,7 +96,7 @@ const Home = (props) => {
                     demographics to get unbiased feedback on your favorite names.
                     Also receive Trademark support from our team of Licensed Trademark Attorneys,
                     so you can pick your name with confidence.
-</p>
+                  </p>
                 </div>
               </div>
             </div>
@@ -206,9 +208,11 @@ const Home = (props) => {
               images={carouselConstants.exampleSliderImages}
               carouselType={carouselConstants.EXAMPLE_SLIDER}
             />
-            <div className={styles.button}>
-              <Link className={styles.button__link} to="/dashboard">DASHBOARD</Link>
-            </div>
+            {data && 
+              <div className={styles.button}>
+                <Link className={styles.button__link} to="/dashboard">DASHBOARD</Link>
+              </div>
+            }
             <div className={styles.blueContainer}>
               <h2 className={styles.whiteUnderline}>What our customers say</h2>
               <SlideBar
@@ -225,8 +229,8 @@ const Home = (props) => {
 };
 
 const mapStateToProps = (state) => {
-  const { isFetching } = state.userStore;
-  return { isFetching };
+  const { isFetching, data } = state.userStore;
+  return { isFetching, data };
 };
 
 export default connect(mapStateToProps, null)(Home);
