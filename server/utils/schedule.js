ec42e399bbaf3043eb2e4ab53717273faa0876a5
LitvE <yevgen.litvinov1980@gmail.com>
2022-11-23 22:23:16 +0100

    Task 8 Logger creation, changed for node-cron, new period - every 45 minutes

--- server/utils/schedule.js
+++ server/utils/schedule.js
@@ -1,61 +1,71 @@
-const schedule = require('node-schedule');
-const fs = require('fs');
+const cron = require("node-cron");
+const fs = require("fs");
 
 module.exports.jobToSchedule = () => {
-    const rule = new schedule.RecurrenceRule();
-    rule.hour = 23;
-    rule.minute = 59;
-    rule.dayOfWeek = 0;
-    rule.tz = 'Europe/Kiev';
-
-    const job = schedule.scheduleJob(rule, function(){
-        fs.readFile('/server/errors/errors.log', (err, data) => {
+  cron.schedule(
+    "*/45 * * * *",
+    () => {
+      fs.readFile("/server/errors/errors.log", (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        } else {
+          fs.stat("/server/errors/errors.log", function(err, stats) {
             if (err) {
-              console.error(err);
-              return
+                console.error(err);
+                return;
             } else {
-                const date = new Date();
-                fs.writeFile(`/server/errors/logs/${date.getTime()}.log`, '', function(err){
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("Файл создан");
-                    }
-                });
-
-                const buffer = data.toString('utf-8');
-                const arrayOfObj = buffer.split('\n');
-
-                arrayOfObj.forEach((item) => {
-                    if(item){
-                        const end = item.indexOf("stackTrace");
-                        const s = item.slice(0, end-2) + "}";
-                        const obj = JSON.parse(s);
-                        const content = {
-                            message: obj.message,
-                            code: obj.code,
-                            time: obj.time,
-                        };
-                        const contentToString = JSON.stringify(content);
-                        
-                        fs.appendFile(`/server/errors/logs/${date.getTime()}.log`, contentToString + '\n', (err) => {
-                            if (err) {
+                if (stats.size) {
+                    const date = new Date();
+                    fs.writeFile(`/server/errors/logs/${date.getTime()}.log`, '', function(err){
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("Файл создан");
+                        }
+                    });
+        
+                    const buffer = data.toString('utf-8');
+                    const arrayOfObj = buffer.split('\n');
+        
+                    arrayOfObj.forEach((item) => {
+                        if(item){
+                            const end = item.indexOf("stackTrace");
+                            const s = item.slice(0, end-2) + "}";
+                            const obj = JSON.parse(s);
+                            const content = {
+                                message: obj.message,
+                                code: obj.code,
+                                time: obj.time,
+                            };
+                            const contentToString = JSON.stringify(content);
+                            
+                            fs.appendFile(`/server/errors/logs/${date.getTime()}.log`, contentToString + '\n', (err) => {
+                                if (err) {
+                                console.error(err);
+                                return
+                                }
+                            })
+                        }
+                    });
+        
+                    fs.truncate('/server/errors/errors.log', (err) => {
+                        if (err) {
                             console.error(err);
                             return
-                            }
-                        })
-                    }
-                });
-
-                fs.truncate('/server/errors/errors.log', (err) => {
-                    if (err) {
-                        console.error(err);
-                        return
-                    }
-                    console.log("Old file is cleared!");
-                });
-            }  
-        });
-    })
-    return job;
-}
+                        }
+                        console.log("Old file is cleared!");
+                    });
+                } 
+            }
+          });
+        }
+      });
+      //console.log("Schedule is working!")
+    },
+    {
+      scheduled: true,
+      timezone: "Europe/Berlin",
+    }
+  );
+};
